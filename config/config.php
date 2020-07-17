e019f2406b426b5d610d51ffcac815dc3cee880f
Artem Kudin <kydin1997@gmail.com>
2020-07-17 10:28:30 +0400

    categories sync

--- config/config.php
+++ config/config.php
@@ -4,6 +4,10 @@
         'models' => [
             'log' => ArtemiyKudin\log\Models\Log::class,
         ],
+        'tables' => [
+            'users' => \Beauty\Modules\Common\Models\User::class,
+            'profiles' => \Beauty\Modules\Common\Models\Profile::class
+        ],
         'table_names' => [
             'logs' => 'logs',
         ],
